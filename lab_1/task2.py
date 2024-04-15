b7edec7534ce63267a50eb15061c741a26242295
bonyfa <arsens818@gmail.com>
2024-04-16 00:38:43 +0400

    Complete decryption funcion  in task 2

--- lab_1/task2.py
+++ lab_1/task2.py
@@ -7,7 +7,7 @@ from read_save_makedict import read_text_from_file,save_text_to_file,json_to_dic
 logging.basicConfig(level=logging.INFO)
 
 SETTINGS = os.path.join("2", "settings2.json")
-ALPHABET = " оиеантсрвмлдякпзыьучжгхфйюбцщэъ"
+
 
 
 def count_the_frequency(text_path: str) -> dict:
@@ -17,12 +17,29 @@ def count_the_frequency(text_path: str) -> dict:
         freq = dict()
         length = len(text)
         for letter in text:
+            letter = letter.upper()
             if letter not in freq:
                 freq[letter] = text.count(letter) / length
         freq = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
         return freq
     except Exception as ex:
+        logging.error(f"Incorrect path - {ex}")
+
+
+def decryption_text(text_path: str, key_path: str) -> str:
+    """The function receives a directory with text and a key and encrypts the text"""
+    try:
+        text = read_text_from_file(text_path)
+        encr_text = text.upper()
+        key = json_to_dict(key_path)
+        keys = key.keys()
+        for k in keys:
+            k.upper()
+            encr_text = encr_text.replace(k, key[k].lower())
+        return encr_text
+    except Exception as ex:
         logging.error(f"Wrong dictionary - {ex}")
+
 
 
 if __name__ == "__main__":
@@ -39,4 +56,9 @@ if __name__ == "__main__":
     frequency_path = os.path.join(
                                settings["fold_task"],
                                settings["frequency"])
-    dict_to_json(frequency_path, count_the_frequency(txt_path))
+    freq = count_the_frequency(txt_path)
+    dict_to_json(key_path, dict(zip(freq.keys(), settings["alphabet"])))
+    print(freq.keys())
+    print(settings["alphabet"])
+    save_text_to_file(decryption_text(txt_path, key_path), result_path)
+
