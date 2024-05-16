ffc50c8f00421ccc2fd276a865825ebabdb5d2e0
bonyfa <arsens818@gmail.com>
2024-05-16 18:08:59 +0400

    Create Java generator and fix c++ generator

--- lab_2/Generator.cpp
+++ lab_2/Generator.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#define SIZE 128
-
-
-void generator() {
-    std::srand(static_cast<unsigned>(std::time(0)));
-    for (int i = 0; i < SIZE; ++i) {
-        int random_bit = std::rand() % 2;
-        std::cout << random_bit;
-    }
-}
-
+#include <random>
 
 int main() {
-    generator();
+    // Инициализация генератора случайных чисел
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::uniform_int_distribution<uint64_t> dis;
+
+    // Генерация 128-битной последовательности
+    for (int i = 0; i < 2; ++i) { // Генерируем 2 числа по 64 бита
+        uint64_t randomNum = dis(gen);
+        for (int j = 63; j >= 0; --j) { // Выводим каждый бит числа
+            std::cout << ((randomNum >> j) & 1);
+        }
+    }
+    std::cout << std::endl;
+
     return 0;
 }
---
+++ lab_2/generator.java
@@ -0,0 +1,23 @@
+import java.security.SecureRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        // Инициализация генератора случайных чисел
+        SecureRandom secureRandom = new SecureRandom();
+
+        // Генерация 128-битной последовательности
+        byte[] randomBytes = new byte[16]; // 16 байт для 128 бит
+        secureRandom.nextBytes(randomBytes); // Заполнение массива случайными байтами
+
+        // Преобразование массива байтов в двоичную строку
+        StringBuilder binaryStringBuilder = new StringBuilder();
+        for (byte b : randomBytes) {
+            for (int i = 7; i >= 0; i--) {
+                binaryStringBuilder.append((b >> i) & 1);
+            }
+        }
+
+        // Вывод двоичной строки
+        System.out.println(binaryStringBuilder.toString());
+    }
+}
