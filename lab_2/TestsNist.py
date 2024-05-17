30ea32677ba54abc5b45d6f2fbc2528962b1ce46
bonyfa <arsens818@gmail.com>
2024-05-17 13:42:49 +0400

    Finish test_longest_sequence_of_ones

--- lab_2/TestsNist.py
+++ lab_2/TestsNist.py
@@ -2,10 +2,12 @@ import json
 import logging
 import os.path
 from math import erfc, fabs, sqrt, pow
+from mpmath import gammainc
 
 logging.basicConfig(level=logging.INFO)
 
 SETTINGS =("sequence.json")
+PI_I = {0:0.2148, 1:0.3672, 2:0.2305, 3:0.1875}
 
 def bit_test(str: str)-> float:
     try:
@@ -33,12 +35,50 @@ def test_identical_consecutive_bits(str: str) -> float:
         logging.error(f"Wrong str - {ex}")
 
 
+def test_longest_sequence_of_ones(str: str) -> float:
+    try:
+        str1 = list(map(int, sequence))
+        block_size = 8
+        blocks = [str1[i:i + block_size] for i in range(0, len(str1), block_size)]
+        v_i = {1: 0, 2: 0, 3: 0, 4: 0}
+        for block in blocks:
+            once = "1"
+            while once in block:
+                once += "1"
+            if once.count("1") <= 1:
+                v_i[1] += 1
+            if once.count("1") == 2:
+                v_i[1] += 1
+            if once.count("1") == 3:
+                v_i[1] += 1
+            if once.count("1") >= 4:
+                v_i[1] += 1
+        x_square = 0
+        for i in range(4):
+            x_square += pow(v_i[i + 1] - 16 * PI_I[i], 2) / (16 * PI_I[i])
+        p = gammainc(3 / 2, x_square / 2)
+        return p
+    except Exception as ex:
+        logging.error(f"Wrong str - {ex}")
 
 
 if __name__ == "__main__":
     with open(SETTINGS, 'r', encoding='utf-8') as f:
         sequence = json.load(f)
 
+    cpp_seq = sequence['cpp']
+    java_seq = sequence['java']
+
+    with open(RESULT_PATH, 'w', encoding='utf-8') as f:
+        f.write("Results for C++ sequence\n")
+        f.write(str(frequency_bit_test(cpp_seq)) + '\n')
+        f.write(str(identical_consecutive_bits(cpp_seq)) + '\n')
+        f.write(str(longest_sequence_of_ones_in_the_block(cpp_seq)) + '\n')
+
+        f.write("\nResults for Java sequence\n")
+        f.write(str(frequency_bit_test(java_seq)) + '\n')
+        f.write(str(identical_consecutive_bits(java_seq)) + '\n')
+        f.write(str(longest_sequence_of_ones_in_the_block(java_seq)) + '\n')
 
 
 
