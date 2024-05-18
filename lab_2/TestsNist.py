1fa5ab74b45e5082b262024fbdf3810e00b586d5
bonyfa <arsens818@gmail.com>
2024-05-18 13:40:28 +0400

    Done

--- lab_2/TestsNist.py
+++ lab_2/TestsNist.py
@@ -11,7 +11,7 @@ RESULT_PATH = os.path.join("result.txt")
 PI_I = {0:0.2148, 1:0.3672, 2:0.2305, 3:0.1875}
 
 
-def bit_test(sequence: str)-> float:
+def bit_test(sequence: str) -> float:
     try:
         str1 = list(map(float,sequence))
         result_seq = [-1 if x == 0 else x for x in str1]
@@ -22,39 +22,41 @@ def bit_test(sequence: str)-> float:
         logging.error(f"Invalid sequence : {ex}")
 
 
-def test_identical_consecutive_bits(str: str) -> float:
+def test_identical_consecutive_bits(sequence: str) -> float:
     try:
-        str1 = list(str)
-        l = 1/len(str1) * sum(str1)
+        str1 = list(map(float, sequence))
+
+        l = (1/float(len(str1))) * sum(str1)
         if ((fabs(l - 0.5)) >= (2 / sqrt(len(str1)))):
             return 0.0
         Vn = 0
         for i in range (len(str1)-1):
-            Vn += 0 if str1[i] == str[i+1] else 1
+            Vn += 0 if str1[i] == str1[i+1] else 1
         p = erfc((fabs(Vn - 2*len(str1)*l*(1-l)))/(2 * sqrt(2 * len(str1)) * l * (1-l)))
         return p
     except Exception as ex:
         logging.error(f"Wrong str - {ex}")
 
 
-def test_longest_sequence_of_ones(str: str) -> float:
+def test_longest_sequence_of_ones(sequence: str) -> float:
     try:
         str1 = list(map(int, sequence))
         block_size = 8
-        blocks = [str1[i:i + block_size] for i in range(0, len(str1), block_size)]
+        blocks = [str1[i:i + block_size] for i in range(0, len(sequence), block_size)]
         v_i = {1: 0, 2: 0, 3: 0, 4: 0}
         for block in blocks:
-            once = "1"
-            while once in block:
+            once = ""
+            while once in ''.join(map(str,block)):
                 once += "1"
+            once = once[:-1]
             if once.count("1") <= 1:
                 v_i[1] += 1
             if once.count("1") == 2:
-                v_i[1] += 1
+                v_i[2] += 1
             if once.count("1") == 3:
-                v_i[1] += 1
+                v_i[3] += 1
             if once.count("1") >= 4:
-                v_i[1] += 1
+                v_i[4] += 1
         x_square = 0
         for i in range(4):
             x_square += pow(v_i[i + 1] - 16 * PI_I[i], 2) / (16 * PI_I[i])
@@ -75,12 +77,12 @@ if __name__ == "__main__":
         f.write("Results for C++ sequence\n")
         f.write(str(bit_test(cpp_seq)) + '\n')
         f.write(str(test_identical_consecutive_bits(cpp_seq)) + '\n')
-       # f.write(str(test_longest_sequence_of_ones(cpp_seq)) + '\n')
+        f.write(str(test_longest_sequence_of_ones(cpp_seq)) + '\n')
 
         f.write("\nResults for Java sequence\n")
         f.write(str(bit_test(java_seq)) + '\n')
         f.write(str(test_identical_consecutive_bits(java_seq)) + '\n')
-        #f.write(str(test_longest_sequence_of_ones(java_seq)) + '\n')
+        f.write(str(test_longest_sequence_of_ones(java_seq)) + '\n')
 
 
 
