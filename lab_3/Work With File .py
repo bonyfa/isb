f974695d13b7daeac75dc7c4d566be1c7c116df6
bonyfa <arsens818@gmail.com>
2024-05-30 20:19:03 +0400

    REad and write file in bytes

--- lab_3/Work With File .py
+++ lab_3/Work With File .py
@@ -9,8 +9,11 @@ logging.basicConfig(level=logging.INFO)
 
 
 def read_data(path: str) -> str:
+    """
+       Read data from file(in bytes)
+       """
     try:
-        with open(path, 'w', encoding='utf-8') as file:
+        with open(path, 'r', encoding='utf-8') as file:
             data = file.read()
         return data
     except Exception as ex:
@@ -18,8 +21,34 @@ def read_data(path: str) -> str:
 
 
 def write_data(path: str, text: str) -> None:
+    """
+       Write data in file(in bytes)
+       """
     try:
         with open(path, 'w', encoding='utf-8') as file:
+            file.write(text)
+    except Exception as ex:
+        logging.error(ex)
+
+
+def read_data_bytes(path: str) -> bytes:
+    """
+       Read data from file(in bytes)
+       """
+    try:
+        with open(path, 'rb', encoding='utf-8') as file:
+            data = file.read()
+        return data
+    except Exception as ex:
+        logging.error(ex)
+
+
+def write_data_bytes(path: str, text: bytes) -> None:
+    """
+       Write data in file(in bytes)
+       """
+    try:
+        with open(path, 'wb', encoding='utf-8') as file:
             file.write(text)
     except Exception as ex:
         logging.error(ex)
